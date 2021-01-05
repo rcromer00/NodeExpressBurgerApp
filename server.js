01ee02d5cdf10b391d562fb4c3126611020803dd
rcromer00 <cromerro@msu.edu>
2021-01-05 11:39:00 -0500

    server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
+
 
 const app = express();
 
@@ -21,31 +21,4 @@ app.use("/", routes);
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT)
-});
-
-=======
-const express = require("express");
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-
-const PORT = process.env.PORT || 8080;
-
-const app = express();
-
-app.use(express.static("public"));
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controller.js");
-
-app.use("/", routes);
-
-app.listen(PORT, () => {
-    console.log("Server listening on: http://localhost:" + PORT)
-});
-
->>>>>>> 9e99881 (Upload Commit)
+}); 
